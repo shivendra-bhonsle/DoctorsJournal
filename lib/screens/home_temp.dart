74d12f8af0f09ff8274144c7093f8d5ba260b741
Rajas <rajaschitale@gmail.com>
2021-06-16 15:53:27 +0530

    Splash screen created.

---
+++ lib/screens/home_temp.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomeTemp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Doctor's Diary"),
+        backgroundColor: Colors.indigo[900],
+      ),
+    );
+  }
+}
---
+++ lib/shared/splash_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class StartLoading extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Image(
+        image: AssetImage('images/Android - 1spalshscreen_figma.png'),
+      )
+    );
+  }
+}

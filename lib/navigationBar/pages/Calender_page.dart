11a789cb3e1afc7a244df61abf94d794f244f11d
Varad Patwardhan <varadp2000@gmail.com>
2021-06-21 23:18:40 +0530

    Added calendar

--- lib/navigationBar/pages/Calender_page.dart
+++ lib/navigationBar/pages/Calender_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class Calender extends StatelessWidget {
   const Calender({Key? key}) : super(key: key);
@@ -11,6 +12,60 @@ class Calender extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.blue,
       ),
+      body: Calendar(),
+    );
+  }
+}
+
+class Calendar extends StatefulWidget {
+  const Calendar({Key? key}) : super(key: key);
+
+  @override
+  _CalendarState createState() => _CalendarState();
+}
+
+class _CalendarState extends State<Calendar> {
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: TableCalendar(
+          firstDay: DateTime.utc(2010, 10, 16),
+          lastDay: DateTime.utc(2030, 3, 14),
+          focusedDay: _focusedDay,
+          calendarFormat: _calendarFormat,
+          onFormatChanged: (format) {
+            if (_calendarFormat != format) {
+              setState(() {
+                _calendarFormat = format;
+              });
+            }
+          },
+          selectedDayPredicate: (day)
+          {
+            if (_selectedDay == day) {
+              return true;
+            }
+            else {
+              return false;
+            }
+          },
+          onDaySelected: (selectedDay, focusedDay) {
+            if (_selectedDay != selectedDay) {
+              setState(() {
+                _selectedDay = selectedDay;
+                _focusedDay = focusedDay;
+              });
+            }
+          },
+          onPageChanged: (focusedDay)
+          {
+            _focusedDay = focusedDay;
+          },
+        ),
     );
   }
 }
